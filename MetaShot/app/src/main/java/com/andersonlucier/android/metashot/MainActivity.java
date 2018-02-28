9981b8aca37a6e80a23d2116938ccfeda253f2ad
Glenn Anderson <ganderson02@unomaha.edu>
2018-02-27 19:30:48 -0600

    project setup

---
+++ MetaShot/app/src/main/java/com/andersonlucier/android/metashot/MainActivity.java
@@ -0,0 +1,13 @@
+package com.andersonlucier.android.metashot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
