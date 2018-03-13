b59b914e4a88b0a08fec483810826f911cee58e8
DanielLucier <dlucier117@hotmail.com>
2018-03-13 09:10:55 -0500

    add landing page layout

--- MetaShot/app/src/main/java/com/andersonlucier/android/metashot/MainActivity.java
+++ MetaShot/app/src/main/java/com/andersonlucier/android/metashot/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.landing_page);
 
         Service service = new Service();
         ShootingRecord shooting = service.getShootingRecordById();
