8ba5cb4277743d32fab5fbc9752c2bb87a1fa47b
DanielLucier <dlucier117@hotmail.com>
2018-03-13 18:28:45 -0500

    create new activity

---
+++ MetaShot/app/src/main/java/com/andersonlucier/android/metashot/NewShootingRecord.java
@@ -0,0 +1,13 @@
+package com.andersonlucier.android.metashot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewShootingRecord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_shooting_record);
+    }
+}
