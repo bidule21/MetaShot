ec1e22b8f2ade9392ed3490426fe87121cb94c84
DanielLucier <dlucier117@hotmail.com>
2018-03-25 12:51:25 -0500

    start of view/edit weapon inventory activity

---
+++ MetaShot/app/src/main/java/com/andersonlucier/android/metashot/WeaponInventory.java
@@ -0,0 +1,13 @@
+package com.andersonlucier.android.metashot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeaponInventory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weapon_inventory);
+    }
+}
