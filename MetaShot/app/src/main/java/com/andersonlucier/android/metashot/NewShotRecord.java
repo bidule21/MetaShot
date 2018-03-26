1f0ae4889ffb772b4fd1d7faceef1333c7b8cbee
DanielLucier <dlucier117@hotmail.com>
2018-03-26 16:26:39 -0500

    add new activity and layout

---
+++ MetaShot/app/src/main/java/com/andersonlucier/android/metashot/NewShotRecord.java
@@ -0,0 +1,13 @@
+package com.andersonlucier.android.metashot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewShotRecord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_shot_record);
+    }
+}
