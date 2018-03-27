ccd63dace9d38012f78870ed9052c6dff6ed71cc
DanielLucier <dlucier117@hotmail.com>
2018-03-27 09:30:27 -0500

    add new activity/layout

---
+++ MetaShot/app/src/main/java/com/andersonlucier/android/metashot/NewShotRecord_AutoRecord.java
@@ -0,0 +1,13 @@
+package com.andersonlucier.android.metashot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewShotRecord_AutoRecord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_shot_record_auto_record);
+    }
+}
