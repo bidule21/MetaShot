72ff5e34c54f48a334ef1bcd3558c8e2dbe54ae3
Glenn Anderson <ganderson02@unomaha.edu>
2018-04-15 14:50:40 -0500

    WeaponInventory is now a populated list

--- MetaShot/app/src/main/java/com/andersonlucier/android/metashot/WeaponInventory.java
+++ MetaShot/app/src/main/java/com/andersonlucier/android/metashot/WeaponInventory.java
@@ -4,30 +4,41 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.andersonlucier.android.metashot.databaseservicelib.DatabaseShotService;
 import com.andersonlucier.android.metashot.databaseservicelib.impl.GunRecord;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WeaponInventory extends AppCompatActivity {
 
     private DatabaseShotService dbService;
+    private ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weapon_inventory);
 
+        lv = (ListView) findViewById(R.id.inventoryList);
+
         dbService = new DatabaseShotService(this);
         List<GunRecord> records = dbService.getAllGunRecords();
 
+        List<String> gunArrayList = new ArrayList<>();
         for (GunRecord gun : records){
-            //heres where you add the records to the list
-            System.out.println(gun.id());
-            System.out.println(gun.gunName());
-            System.out.println(gun.details());
+            gunArrayList.add(gun.gunName());
         }
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                gunArrayList );
+
+        lv.setAdapter(arrayAdapter);
 
     }
     public void onClick(View view){
